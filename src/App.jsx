294bd7f3e65d68f476f765009fdf9fcb9fb01e55
Oleksandr Vasylenko <79399963+aalexblade@users.noreply.github.com>
2023-02-13 13:51:07 +0200

    route

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,17 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import { Header } from './components/Layout/Header/Header';
 import { Layout } from './components/Layout/Layout';
-// import CounterPage from './pages/ExercisesPage/CounterPage/CounterPage';
-// import ExercisesPage from './pages/ExercisesPage/ExercisesPage';
-// import LongRequestPage from './pages/ExercisesPage/LongRequestPage/LongRequestPage';
-// import RerenderPage from './pages/ExercisesPage/RerenderPage/RerenderPage';
-// import TimerPage from './pages/ExercisesPage/TimerPage/TimerPage';
+import CounterPage from './pages/ExercisesPage/CounterPage/CounterPage';
+import ExercisesPage from './pages/ExercisesPage/ExercisesPage';
+import LongRequestPage from './pages/ExercisesPage/LongRequestPage/LongRequestPage';
+import RerenderPage from './pages/ExercisesPage/RerenderPage/RerenderPage';
+import TimerPage from './pages/ExercisesPage/TimerPage/TimerPage';
 import HomePage from './pages/HomePage/HomePage';
 import NewPostPage from './pages/NewPostPage/NewPostPage';
-// import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 import PostsListPage from './pages/PostsListPage/PostsListPage';
-// import CommentsPage from './pages/SinglePostPage/CommentsPage/CommentsPage';
-// import SinglePostPage from './pages/SinglePostPage/SinglePostPage';
+import CommentsPage from './pages/SinglePostPage/CommentsPage/CommentsPage';
+import SinglePostPage from './pages/SinglePostPage/SinglePostPage';
 
 export const App = () => {
   return (
@@ -24,15 +24,25 @@ export const App = () => {
 
         <Routes>
           <Route path="" element={<HomePage />} />
+
           <Route path="/posts" element={<PostsListPage />} />
+
+          <Route path="/posts/:postId" element={<SinglePostPage />}>
+            <Route path="comments" element={<CommentsPage />} />
+          </Route>
+
           <Route path="/new-post" element={<NewPostPage />} />
 
-          {/* <CommentsPage /> */}
-          {/* <CommentsPage /> */}
-          {/* <CommentsPage /> */}
-          {/* <CommentsPage /> */}
-          {/* <CommentsPage /> */}
-          {/* <CommentsPage /> */}
+          <Route path="/exercises" element={<ExercisesPage />}>
+            <Route index element={<Navigate to="timer" />} />
+
+            <Route path="timer" element={<TimerPage />} />
+            <Route path="long-request" element={<LongRequestPage />} />
+            <Route path="counter" element={<CounterPage />} />
+            <Route path="re-render" element={<RerenderPage />} />
+          </Route>
+
+          <Route path="*" element={<NotFoundPage />} />
 
         </Routes>
       </Layout>
--- src/pages/PostsListPage/PostsListPage.jsx
+++ src/pages/PostsListPage/PostsListPage.jsx
@@ -1,31 +1,34 @@
 import { useMemo, useEffect, useState } from 'react';
 
 import debounce from 'lodash/debounce';
-import { useDispatch, useSelector } from 'react-redux';
-import { useSearchParams } from 'react-router-dom';
 
 import { NotFound } from '../../components/NotFound/NotFound';
 import { PostsItem } from '../../components/Posts/PostsItem';
 import { PostsLoader } from '../../components/Posts/PostsLoader';
 import { STATUS } from '../../constants/status.constants';
-import { getPostsThunk } from '../../redux/posts/posts.thunk';
+import { getPosts } from '../../services/posts.service';
+import axios from 'axios';
 
 const PostsListPage = () => {
-  const dispatch = useDispatch();
-  const status = useSelector(state => state.posts.status);
-  const posts = useSelector(state => state.posts.posts);
+  const [posts, setPosts] = useState(null);
+  const [status, setStatus] = useState(STATUS.idle);
+  const [search, setSearch] = useState('');
 
-  const [searchParams, setSearchParams] = useSearchParams();
-  const page = searchParams.get('page') ?? 1;
-  const searchQuery = searchParams.get('search') ?? '';
-
-  const [search, setSearch] = useState(searchQuery);
+  const fetchData = async ({ page = 1, query = '' }) => {
+    setStatus(STATUS.loading);
+    try {
+      const data = await getPosts({ page, search: query });
+      setPosts(data);
+      setStatus(STATUS.success);
+    } catch (error) {
+      console.log(error);
+      setStatus(STATUS.error);
+    }
+  };
 
   const searchPosts = useMemo(() => {
-    return debounce(search => {
-      setSearchParams({ page: 1, search }); // localhost.../?page=1&search=javascript
-    }, 500);
-  }, [setSearchParams]);
+    return debounce(query => fetchData({ query }), 500);
+  }, []);
 
   const handleSearch = event => {
     setSearch(event.target.value);
@@ -33,8 +36,19 @@ const PostsListPage = () => {
   };
 
   useEffect(() => {
-    dispatch(getPostsThunk({ page, search: searchQuery }));
-  }, [dispatch, page, searchQuery]);
+    fetchData({ page: 1 });
+  }, []);
+
+  // useEffect(() => {
+  //   const myFunc = async () => {
+  //     // try {
+  //     // const data = await ....
+  //     // }
+  //   };
+
+  //   myFunc();
+  //   axios.get('...').then(data).catch().finally()
+  // }, []);
 
   return (
     <>
@@ -62,20 +76,21 @@ const PostsListPage = () => {
         <div className="pagination">
           <div className="btn-group my-2 mx-auto btn-group-lg">
             {[...Array(posts.total_pages)].map((_, index) => {
-              const innerPage = index + 1;
+              const page = index + 1;
 
               return (
                 <button
                   key={index}
                   type="button"
-                  disabled={innerPage === posts.page}
+                  disabled={page === posts.page}
                   className="btn btn-primary"
                   onClick={() => {
-                    // setSearchParams({ page: 2, data: 'data', test: 'test' }); // '?page=2&data=data&test=test'
-                    setSearchParams({ page: innerPage, search: searchQuery });
+                    if (page !== posts.page) {
+                      fetchData({ page, query: search });
+                    }
                   }}
                 >
-                  {innerPage}
+                  {page}
                 </button>
               );
             })}
@@ -87,3 +102,94 @@ const PostsListPage = () => {
 };
 
 export default PostsListPage;
+// ===========================================
+
+// import { useMemo, useEffect, useState } from 'react';
+
+// import debounce from 'lodash/debounce';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { useSearchParams } from 'react-router-dom';
+
+// import { NotFound } from '../../components/NotFound/NotFound';
+// import { PostsItem } from '../../components/Posts/PostsItem';
+// import { PostsLoader } from '../../components/Posts/PostsLoader';
+// import { STATUS } from '../../constants/status.constants';
+// import { getPostsThunk } from '../../redux/posts/posts.thunk';
+
+// const PostsListPage = () => {
+//   const dispatch = useDispatch();
+//   const status = useSelector(state => state.posts.status);
+//   const posts = useSelector(state => state.posts.posts);
+
+//   const [searchParams, setSearchParams] = useSearchParams();
+//   const page = searchParams.get('page') ?? 1;
+//   const searchQuery = searchParams.get('search') ?? '';
+
+//   const [search, setSearch] = useState(searchQuery);
+
+//   const searchPosts = useMemo(() => {
+//     return debounce(search => {
+//       setSearchParams({ page: 1, search }); // localhost.../?page=1&search=javascript
+//     }, 500);
+//   }, [setSearchParams]);
+
+//   const handleSearch = event => {
+//     setSearch(event.target.value);
+//     searchPosts(event.target.value);
+//   };
+
+//   useEffect(() => {
+//     dispatch(getPostsThunk({ page, search: searchQuery }));
+//   }, [dispatch, page, searchQuery]);
+
+//   return (
+//     <>
+//       <input
+//         type="text"
+//         className="form-control mb-4"
+//         placeholder="Type to search..."
+//         value={search}
+//         onChange={handleSearch}
+//       />
+
+//       {(status === STATUS.loading || status === STATUS.idle) && <PostsLoader />}
+
+//       {status === STATUS.error && <NotFound />}
+
+//       <div className="container-fluid g-0 pb-5 mb-5">
+//         <div className="row">
+//           {posts?.data.map(post => (
+//             <PostsItem key={post.id} post={post} />
+//           ))}
+//         </div>
+//       </div>
+
+//       {posts?.total_pages && (
+//         <div className="pagination">
+//           <div className="btn-group my-2 mx-auto btn-group-lg">
+//             {[...Array(posts.total_pages)].map((_, index) => {
+//               const innerPage = index + 1;
+
+//               return (
+//                 <button
+//                   key={index}
+//                   type="button"
+//                   disabled={innerPage === posts.page}
+//                   className="btn btn-primary"
+//                   onClick={() => {
+//                     // setSearchParams({ page: 2, data: 'data', test: 'test' }); // '?page=2&data=data&test=test'
+//                     setSearchParams({ page: innerPage, search: searchQuery });
+//                   }}
+//                 >
+//                   {innerPage}
+//                 </button>
+//               );
+//             })}
+//           </div>
+//         </div>
+//       )}
+//     </>
+//   );
+// };
+
+// export default PostsListPage;
