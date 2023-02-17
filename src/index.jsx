3d3aff1cb28c84083a7f3cf5c5108a997d001167
Oleksandr Vasylenko <79399963+aalexblade@users.noreply.github.com>
2023-02-17 13:56:17 +0200

    redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,41 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 
 import { App } from './App';
-import { store, persistor } from './redux/store';
 
 import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
+import './redux/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
+  <Provider>
+    <PersistGate>
       <App />
     </PersistGate>
   </Provider>,
 );
+
+// import React from 'react';
+
+// import ReactDOM from 'react-dom/client';
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+
+// import { App } from './App';
+// import { store, persistor } from './redux/store';
+
+// import 'react-toastify/dist/ReactToastify.css';
+// import './index.css';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <Provider store={store}>
+//     <PersistGate loading={null} persistor={persistor}>
+//       <App />
+//     </PersistGate>
+//   </Provider>,
+// );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,45 +1,66 @@
-import { configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
+import { createStore } from 'redux';
 
-import { counterInitState } from './counter/counter.init-state';
-import { counterReducer } from './counter/counter.reducer';
-import { postsInitState } from './posts/posts.init-state';
-import { postsReducer } from './posts/posts.slice';
-import { postsApi } from './rtk-posts/rtk-posts.api';
-import { userInitState } from './users/users.init-state';
-import { usersReducer } from './users/users.slice';
+const reducer = (state, action) => {
+  // console.log(state, action);
+  if (action.type === 'PLUSE') {
+    return { counter: state.counter + 1 };
+  }
 
-const initState = {
-  counter: counterInitState,
-  users: userInitState,
-  posts: postsInitState,
+  if (action.type === 'MINUS') {
+    return { counter: state.counter - 1 };
+  }
+  return state;
 };
+const store = createStore(reducer, { counter: 0 });
 
-export const store = configureStore({
-  preloadedState: initState,
-  devTools: true,
-  reducer: {
-    counter: counterReducer,
-    users: usersReducer,
-    posts: postsReducer,
+console.log(store.getState());
+store.dispatch({ type: 'PLUSE' });
+store.dispatch({ type: 'MINUS' });
+console.log(store.getState());
 
-    [postsApi.reducerPath]: postsApi.reducer,
-  },
+// ===========================================
+// import { configureStore } from '@reduxjs/toolkit';
+// import {
+//   persistStore,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
 
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }).concat([postsApi.middleware]),
-});
+// import { counterInitState } from './counter/counter.init-state';
+// import { counterReducer } from './counter/counter.reducer';
+// import { postsInitState } from './posts/posts.init-state';
+// import { postsReducer } from './posts/posts.slice';
+// import { postsApi } from './rtk-posts/rtk-posts.api';
+// import { userInitState } from './users/users.init-state';
+// import { usersReducer } from './users/users.slice';
 
-export const persistor = persistStore(store);
+// const initState = {
+//   counter: counterInitState,
+//   users: userInitState,
+//   posts: postsInitState,
+// };
+
+// export const store = configureStore({
+//   preloadedState: initState,
+//   devTools: true,
+//   reducer: {
+//     counter: counterReducer,
+//     users: usersReducer,
+//     posts: postsReducer,
+
+//     [postsApi.reducerPath]: postsApi.reducer,
+//   },
+
+//   middleware: getDefaultMiddleware =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }).concat([postsApi.middleware]),
+// });
+
+// export const persistor = persistStore(store);
